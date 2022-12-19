ffb41b2a2bedadfb210fd048a846603b91025829
Shishkin Aleksandr <weltkind9@yandex.ru>
2022-12-20 02:26:57 +0400

    deploy demo_bot

--- deploy/demo_bot/app.py
+++ deploy/demo_bot/app.py
@@ -27,27 +27,30 @@ def handle_photo(message):
     if message.photo is None:
         bot.send_message(message.from_user.id, "Отправьте, пожалуйста картинку")   
     else:
-        fileID = message.photo[-1].file_id   
-        file_info = bot.get_file(fileID)
-        
-        downloaded_file = bot.download_file(file_info.file_path)
-        filename = datetime.datetime.now().strftime("%d.%m.%Y_%H.%M.%S.%f")
-        with open(f'{filename}.png', 'wb') as f:
-            f.write(downloaded_file)
-        with open(f'{filename}.png', 'rb') as f:
-            resp = requests.post(url=url_ocr, files={'file': f})
-        
-        text = resp.json()['message']
-        img = Image.open(f'{filename}.png')
-        
-        with open(f'{filename}.pkl', 'wb') as f:
-            pickle.dump((img, text), f)
+        try:
+            fileID = message.photo[-1].file_id   
+            file_info = bot.get_file(fileID)
+            
+            downloaded_file = bot.download_file(file_info.file_path)
+            filename = datetime.datetime.now().strftime("%d.%m.%Y_%H.%M.%S.%f")
+            with open(f'{filename}.png', 'wb') as f:
+                f.write(downloaded_file)
+            with open(f'{filename}.png', 'rb') as f:
+                resp = requests.post(url=url_ocr, files={'file': f})
+            
+            text = resp.json()['message']
+            img = Image.open(f'{filename}.png')
+            
+            with open(f'{filename}.pkl', 'wb') as f:
+                pickle.dump((img, text), f)
 
-        with open(f'{filename}.pkl', 'rb') as f:
-            resp = requests.post(url=url_predict, files={'file': f})
-        result = f'{text}\n{resp.json()["message"]}'
-        
-        bot.send_message(message.from_user.id, result)
+            with open(f'{filename}.pkl', 'rb') as f:
+                resp = requests.post(url=url_predict, files={'file': f})
+            result = f'{text}\n{resp.json()["message"]}'
+            
+            bot.send_message(message.from_user.id, result)
+        except Exception as ex:
+            bot.send_message(message.from_user.id, "Internal error")
     
     
 bot.infinity_polling()
